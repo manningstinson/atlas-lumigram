b5943c2fde66afa173b1597cf2d3181217e044e2
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-25 19:10:48 +0000

    changed Home Feed, to just Home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,7 +10,7 @@ export default function TabsLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: "Home Feed",
+          title: "Home",
           tabBarIcon: ({ color }) => (
             <Ionicons name="home" size={24} color={color} />
           ),
