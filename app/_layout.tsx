f331a7ab9b3346c03ad7a530e5e2e587ddfb8eb7
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-03-05 18:25:34 +0000

    updated ImageGettureHandler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,28 @@
 // app/_layout.tsx
 import React from 'react';
 import { Stack } from 'expo-router';
-import { AuthProvider } from '../app/contexts/auth.context';
-import { AuthProtection } from '../components/AuthProtection';
+import { AuthProvider } from './contexts/auth.context';
+import { AuthProtection } from './../components/AuthProtection';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <AuthProtection>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        </Stack>
-      </AuthProtection>
-    </AuthProvider>
+    <GestureHandlerRootView style={styles.container}>
+      <AuthProvider>
+        <AuthProtection>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack>
+        </AuthProtection>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
